01fb02f034c2375e0f7cb9124f1f54533b0a141d
youxiaolong <1494032@qq.com>
2018-07-31 14:40:14 +0800

    * 增加查找文件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +14,14 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this,tr("文件选择"), "",tr("test(*.cpp)"));
+    qDebug()<<filenames;
+    if( !filename.isEmpty())
+    {
+
+    }
 }
